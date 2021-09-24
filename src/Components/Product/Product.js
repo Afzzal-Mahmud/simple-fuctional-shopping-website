add1c0b674f70c347f973bf45bc817a49bf5faa7
afzal mahmud <afzzalmahmud@gmail.com>
2021-09-24 20:21:09 +0600

    add dynamic star icon

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./Product.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart ,faStar} from '@fortawesome/free-solid-svg-icons';
+import StarRatingComponent from "react-star-rating-component";
 function Product(props) {
     const {name,img,category,price,stock,seller,star} = props.productsObject;
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
-    const starIcon = <FontAwesomeIcon style={{color:'goldenrod'}} icon={faStar} />
+    // const starIcon = <FontAwesomeIcon style={{color:'goldenrod'}} icon={faStar} />
     return(
         <div className="product-container">
             <div className="product-img">
@@ -15,7 +16,14 @@ function Product(props) {
                 <h3>Name : {name}</h3>
                 <h4>price :{price} </h4>
                 <p>Catagori :{category}</p>
-                <span><small>Stock : {stock} </small><small> , Seller : {seller} </small> <small>, Ratting : {star} {starIcon}</small></span>
+                <span><small>Stock : {stock} </small><small> , Seller : {seller} </small> 
+                <small>, 
+                Ratting : 
+                {star} <StarRatingComponent
+                name={'ratingStar'}
+                value={star}
+                starCount={5}/></small>
+                </span>
                 <button onClick={()=> props.addToCart(props.productsObject)} className="cart-btn">{cartIcon} Add to Cart</button>
             </div>
         </div>
